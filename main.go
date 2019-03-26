89b3754f044fdbb43c2ff321f8d6f89daef0ff70
Yusuke Mito <y310.1984@gmail.com>
2019-03-26 22:56:06 +0900

    Implement tcsv command

--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"gopkg.in/urfave/cli.v1" // imports as package "cli"
+	"log"
+	"os"
+	"strings"
 )
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "tcsv"
+	app.Usage = "transpose stdin and join by comma"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "separator, s",
+			Value: ",",
+			Usage: "separator to join strings",
+		},
+	}
+	app.Action = func(c *cli.Context) error {
+		stdin := bufio.NewScanner(os.Stdin)
+		values := make([]string, 0)
+		for stdin.Scan() {
+			text := stdin.Text()
+			values = append(values, text)
+		}
+		fmt.Println(strings.Join(values, c.String("separator")))
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
